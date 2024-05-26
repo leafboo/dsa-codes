0a4316338981521259b4f71cf2bcff21d5c243a7
Jan Romel <janjancaranguian@gmail.com>
2024-05-26 22:05:12 +0800

    Update palindrome_number

--- palindrome_number.py
+++ palindrome_number.py
@@ -1,5 +1,15 @@
-def is_palindrome():
-  pass
+def isPalindrome(x):
 
+  temporary_number = x
+  compare_number = 0
+  
+  while temporary_number > 0:
+    compare_number = (compare_number * 10) + (temporary_number % 10)
+    temporary_number = temporary_number // 10
 
-print(is_palindrome(11211))
+  if compare_number == x:
+    return True
+  return False
+
+print(isPalindrome(323))
+    

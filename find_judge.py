6449513e19fe8cd4e0065bf673aa419e5cf236ed
Jan Romel <janjancaranguian@gmail.com>
2024-05-30 20:09:37 +0800

    Continue find_judge

--- find_judge.py
+++ find_judge.py
@@ -1,7 +1,16 @@
 def find_judge(n, trust):
-  pass
+  # (outgoing)
+  outgoing = {i: 0 for i in range(1, n + 1)}
+  # (incoming)
+  incoming = {i: 0 for i in range(1, n + 1)}
+
+  for i in range(len(trust)):
+    outgoing[trust[i][0]] += 1
+    incoming[trust[i][1]] += 1
+  
+    
+
 
 n = 3
 trust = [[1, 3], [2, 3]]
-
 print(find_judge(n, trust))
